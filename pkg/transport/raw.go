a31b24c949688fd365981673765e3d697b89c0c9
mrcgq <pennymrs413@gmail.com>
2026-03-04 11:24:30 +0800

    Create raw.go

---
+++ pkg/transport/raw.go
@@ -0,0 +1,22 @@
+package transport
+
+import "net"
+
+// RawTransport 裸 TLS 传输
+type RawTransport struct{}
+
+func (t *RawTransport) Name() string         { return "raw" }
+func (t *RawTransport) ALPNProtos() []string { return []string{"http/1.1"} }
+
+func (t *RawTransport) Info() TransportInfo {
+	return TransportInfo{
+		SupportsMultiplex: false,
+		SupportsBinary:    true,
+		RequiresUpgrade:   false,
+		MaxFrameSize:      0,
+	}
+}
+
+func (t *RawTransport) Wrap(conn net.Conn, _ *Config) (net.Conn, error) {
+	return conn, nil
+}
